569a677b984e2ccc293d49e6181b56359cfa0405
MEEP of Faith <meepmatthew@gmail.com>
2020-04-23 01:20:40 -0700

    shoot me... please...
    haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah haah feels so good

--- scripts/units/bombardment.js
+++ scripts/units/bombardment.js
@@ -1,4 +1,4 @@
-var shots = 0;
+var shots = 3;
 const vec = new Vec2();
 
 const deffst = extend(MissileBulletType, {})
@@ -8,6 +8,7 @@ deffst.backColor = Color.valueOf("f68021");
 deffst.trailColor = Color.valueOf("d06b53");
 deffst.speed = 3.9;
 deffst.damage = 720;
+deffst.drag = -0.02;
 deffst.splashDamageRadius = 120;
 deffst.splashDamage = 700;
 deffst.bulletWidth = 16;
@@ -21,7 +22,7 @@ deffst.lifetime = 104; //About 50 blocks travel distancthis.
 const satelite = extendContent(UnitType, "bombardment", {
   update(){
 		this.super$update();
-    if(this.getShootTimer(true) ==0){
+    if(this.getShootTimer(true)==2){
       this.shots ++;
       if(this.shots > 3){
         this.shots = 0;
