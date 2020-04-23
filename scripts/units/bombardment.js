2df1c3f9392804420c5a47e33442eb21ef7b71d1
MEEP of Faith <meepmatthew@gmail.com>
2020-04-22 20:49:07 -0700

    Scripty boi

--- scripts/units/bombardment.js
+++ scripts/units/bombardment.js
@@ -2,36 +2,45 @@ const entityLib = this.global.entityLib;
 
 var shots = 0;
 
-const bM = extend(MissileBulletType, {})
-bM.bulletSprite = "shell";
-bM.frontColor = Color.valueOf("f8ad42");
-bM.backColor = Color.valueOf("f68021");
-bM.trailColor = Color.valueOf("d06b53");
-bM.speed = 3.9;
-bM.damage = 720;
-bM.splashDamageRadius = 120;
-bM.splashDamage = 700;
-bM.bulletWidth = 16;
-bM.bulletHeight = 20;
-bM.bulletShrink = 0
-bM.keepVelocity = false;
-bM.despawnEffect = Fx.flakExplosionBig;
-bM.hitEffect = Fx.flakExplosionBig;
-bM.lifetime = 104; //About 50 blocks travel distance.
+const deffst = extend(MissileBulletType, {})
+deffst.bulletSprite = "shell";
+deffst.frontColor = Color.valueOf("f8ad42");
+deffst.backColor = Color.valueOf("f68021");
+deffst.trailColor = Color.valueOf("d06b53");
+deffst.speed = 3.9;
+deffst.damage = 720;
+deffst.splashDamageRadius = 120;
+deffst.splashDamage = 700;
+deffst.bulletWidth = 16;
+deffst.bulletHeight = 20;
+deffst.bulletShrink = 0
+deffst.keepVelocity = false;
+deffst.despawnEffect = Fx.flakExplosionBig;
+deffst.hitEffect = Fx.flakExplosionBig;
+deffst.lifetime = 104; //About 50 blocks travel distance.
 
-const satelite = entityLib.extendUnit(HoverUnit, "bombardment", [{
-    onShoot(e, num, e.x, e.y, e.rotation){
-        shots ++;
-        if(shots > 3){
-            shots = 0;
-            
-            Bullet.create(bM, e, e.getTeam(), e.x, e.y, e.rotation);
-            
-            //shoot effect
-            Draw.color(Color.valueOf("eba313"), Color.valueOf("f28a2e"), Color.valueOf("696969"), e.fin());
-            Angles.randLenVectors(e.id, 10, e.finpow() * 70, e.rotation, 10, (x, y) => {
-                Fill.circle(e.x + x, e.y + y, 0.65 + e.fout() * 1.6);
-            });
-        }
+const satelite = extendContent(HoverUnit, "bombardment", [{
+    if(isDead()){
+      for(var yes = 0; yes < 360; yes += 36){
+        Bullet.create(deffst, owner, owner.getTeam(), x + Tmp.v1.x, y + Tmp.v1.y, yes + Mathf.random(-15,15), (1 - velocityRnd) + Mathf.random(velocityRnd));
+      }
+      remove();
+      return;
+    }
+    if(shooter.getTimer().get(shooter.getShootTimer(left), reload)){
+      shots ++;
+      if(shots > 3){
+        shots = 0;
+        
+        if(owner == null) return;
+        Tmp.v1.trns(angle, 3);
+        Bullet.create(this.deffst, owner, owner.getTeam(), x + Tmp.v1.x, y + Tmp.v1.y, angle, (1 - velocityRnd) + Mathf.random(velocityRnd));
+        
+        //shoot effect
+        Draw.color(Color.valueOf("eba313"), Color.valueOf("f28a2e"), Color.valueOf("696969"), e.fin());
+        Angles.randLenVectors(e.id, 10, e.finpow() * 70, e.rotation, 10, (x, y) => {
+            Fill.circle(e.x + x, e.y + y, 0.65 + e.fout() * 1.6);
+        });
+      }
     }
 }]);
