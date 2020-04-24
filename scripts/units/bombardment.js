0693e6c197d0a67ec1db5f9723d676af717214c6
MEEP of Faith <meepmatthew@gmail.com>
2020-04-24 16:20:28 -0700

    boom

--- scripts/units/bombardment.js
+++ scripts/units/bombardment.js
@@ -22,10 +22,10 @@ deffst.speed = 3.9;
 deffst.damage = 1500;
 deffst.drag = -0.05;
 deffst.splashDamageRadius = 120;
-deffst.splashDamage = 1420;
+deffst.splashDamage = 1450;
 deffst.bulletWidth = 16;
 deffst.bulletHeight = 20;
-deffst.bulletShrink = 0
+deffst.bulletShrink = 0;
 deffst.keepVelocity = false;
 deffst.despawnEffect = Fx.flakExplosionBig;
 deffst.hitEffect = Fx.flakExplosionBig;
@@ -34,13 +34,45 @@ deffst.collides = true;
 deffst.collidesTiles = true;
 deffst.collidesAir = true;
 
+const flamingdebris = newEffect(45, e => {
+	Draw.color(Color.valueOf("#ffffff"), Color.valueOf("#e68b02"), e.fin());
+    const d = new Floatc2({get(x, y){
+    Fill.circle(e.x + x, e.y + y, 0.25 + e.fin() * 0.5);
+    }})
+    Angles.randLenVectors(e.id, 6, -10 + 40 * e.fin(), e.rotation + 180, 360 * e.fin(),d);
+	Draw.color(Color.valueOf("#ffffff"), Color.valueOf("#e68b02"), e.fout());
+    Angles.randLenVectors(e.id, 6, -10 + 40 * e.fout(), e.rotation, 360 * e.fout(),d);
+});
+const deathblast = extend(ArtilleryBulletType,{})
+deathblast.bulletSprite = "shell";
+deathblast.frontColor = Color.valueOf("f8ad42");
+deathblast.backColor = Color.valueOf("f68021");
+deathblast.trailColor = Color.valueOf("d06b53");
+deathblast.trailEffect = flamingdebris;
+deathblast.speed = 3.9;
+deathblast.damage = 450;
+deathblast.drag = -0.05;
+deathblast.splashDamageRadius = 40;
+deathblast.splashDamage = 420;
+deathblast.bulletWidth = 16;
+deathblast.bulletHeight = 20;
+deathblast.bulletShrink = 0;
+deathblast.drawSize = 0;
+deathblast.keepVelocity = false;
+deathblast.despawnEffect = Fx.flakExplosionBig;
+deathblast.hitEffect = Fx.flakExplosionBig;
+deathblast.lifetime = 62; //About 30 blocks travel distance.
+deathblast.collides = true;
+deathblast.collidesTiles = true;
+deathblast.collidesAir = true;
+
 const satelite = extendContent(UnitType, "bombardment", {});
 satelite.create(prov(() => new JavaAdapter(HoverUnit, {
   onDeath(){
     this.super$onDeath();
-    for(var yes = 0; yes < 360; yes += 5){
+    for(var yes = 0; yes < 720; yes ++){
       vec.trns(0, 0, -4);
-      Calls.createBullet(deffst, this.getTeam(), this.x, this.y + vec.y, yes + Mathf.random(-10,10), (0.8) + Mathf.random(0.2), 104);
+      Calls.createBullet(deathblast, this.getTeam(), this.x, this.y + vec.y, yes + Mathf.random(-10,10), (0.8) + Mathf.random(0.2), 104);
     }
   },
   behavior(){
